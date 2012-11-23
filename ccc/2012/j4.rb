44993fbca824032b10e538755d864279cef89cc5
Neil Parikh <parikh.neil@me.com>
2012-11-22 18:05:53 -0700

    Write ccc 2012 Junior problem 4

---
+++ ccc/2012/j4.rb
@@ -0,0 +1,17 @@
+# all test cases passed
+
+k = gets.to_i
+word = gets.chomp
+final = []
+letters = [*'A'..'Z']
+
+word.split('').each_with_index do |c, i|
+  s = k + (i+1) * 3
+  a = letters.index(c) - s
+  while a < 0
+    a = 26 + a
+  end
+  final << letters[a]
+end
+
+puts final.join
